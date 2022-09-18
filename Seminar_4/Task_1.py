ddf19932b77913221433c47856655a957b0dc5a6
Samorodov Sergey <samorodovsv@yandex.ru>
2022-09-18 18:31:16 +0300

    Seminar_4 добавил 3 и 4  задачи

--- Seminar_4/Task_1.py
+++ Seminar_4/Task_1.py
@@ -6,19 +6,6 @@ from math import pi
 
 n = pi
 print(n)
-
-# Формула Бэйли — Боруэйна — Плаффа
-
-n = 100
+n = int(input("Введите число: "))
 my_pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
-
 print(my_pi)
-
-# Ряд Лейбница
-
-# n = 100
-
-# mypi = 4 * (sum(1/x for x in range(1, n + 1, 4)) +
-#             sum(-1/x for x in range(3, n + 1, 4)))
-
-# print(format(mypi, '.100'))
