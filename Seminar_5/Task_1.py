7f81f67dd43ef022bc3b5039d1d5975ef3f5b75f
Samorodov Sergey <samorodovsv@yandex.ru>
2022-09-18 19:44:49 +0300

    Seminar_5 добавил тектовый файл

--- Seminar_5/Task_1.py
+++ Seminar_5/Task_1.py
@@ -1,10 +1,18 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-with open("file_4.txt", "r") as fin:
-     for line in fin:
-        file_4 = line.split()
-        for word in file_4:
-            if "абв" in word:
-               file_4.remove(word)
-        sentence = " ".join(file_4)
-        print(sentence)
+with open('file.txt', 'r') as fin:
+   for line in fin:
+      file = line.split()
+      for word in file:
+         if 'абв' in word:
+            file.remove(word)
+      sentence = ' '.join(file)
+      print(sentence)
+# with open("file.txt", "r") as fin:
+#      for line in fin:
+#         file = line.split()
+#         for word in file:
+#             if "абв" in word:
+#                file.remove(word)
+#         sentence = " ".join(file)
+#         print(sentence)
