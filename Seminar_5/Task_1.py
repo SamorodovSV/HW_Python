ec0b0973595a2ce5adddff443389947f16ab8d5e
Samorodov Sergey <samorodovsv@yandex.ru>
2022-09-18 19:24:48 +0300

    Seminar_5 добавил тектовый файл

--- Seminar_5/Task_1.py
+++ Seminar_5/Task_1.py
@@ -2,9 +2,9 @@
 
 with open("file_4.txt", "r") as fin:
      for line in fin:
-        words = line.split()
-        for word in words:
+        file_4 = line.split()
+        for word in file_4:
             if "абв" in word:
-               words.remove(word)
-        sentence = " ".join(words)
+               file_4.remove(word)
+        sentence = " ".join(file_4)
         print(sentence)
